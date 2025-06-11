36f9e020306a6a0b91a5b7f62602897e218e31ba
John Doe <nicolasromerog.97@gmail.com>
2025-06-11 11:30:26 -0500

    Create a custom hook to fetch the games

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from './components/ui/provider';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import App from "./App";
+import theme from "./theme";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
